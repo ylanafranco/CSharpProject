4fb9043ec23cd7d687b92eb3b826c044b2461930
Ylana <Ylana@DESKTOP-IK2VQO0>
2019-11-03 11:45:25 +0200

    Add project files.

---
+++ dotNet5780_01_4758_7791/Part_1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace dotNet5780_01_4758_7791
+{
+    public class Part_1
+    {
+        int[] A;
+        int[] B;
+        int[] C;
+        public Part_1()
+        {
+            A = new int[20];
+            B = new int[20];
+            C = new int[20];
+
+        }
+
+        public void hagrel(Random r)
+        {
+            for (int i = 0; i < A.Length; i++)
+            {
+                A[i] = r.Next(18, 122);
+                B[i] = r.Next(18, 122);
+                C[i] = Math.Abs( A[i] - B[i]);
+            }
+        }
+
+        public void showResults()
+        {
+            for (int i = 0; i < A.Length; i++)
+            {
+                Console.Write("{0,3} ", A[i]);
+
+            }
+            Console.WriteLine();
+            for (int i = 0; i < B.Length; i++)
+            {
+                Console.Write("{0,3} ", B[i]);
+
+            }
+            Console.WriteLine();
+            for (int i = 0; i < C.Length; i++)
+            {
+                Console.Write("{0,3} ", C[i]);
+
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ dotNet5780_01_4758_7791/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5780_01_4758_7791
+{
+    class Program
+    {
+        static Random r = new Random(DateTime.Now.Millisecond);
+        static void Main(string[] args)
+        {
+            Part_1 t1 = new Part_1();
+            t1.hagrel(r);
+            t1.showResults();
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+        }
+    }
+}
+
+/* OUTPUT
+ *   63  58  45  19  43  56  23 108  49  36  82 101 106  27  24  64  89  55  68  99
+     27  88  83  60  61  93  78  20  94  77 101 101  89  51  35  61 115  97  34  42
+     36  30  38  41  18  37  55  88  45  41  19   0  17  24  11   3  26  42  34  57
+     Press any key to continue */
